03ba6bce91542d37dcabf4d045311e092e041136
Dane Nikolic <danen1995@gmail.com>
2019-10-04 15:15:59 +0200

    Added events to fullcalendar. Handling event click and date click.

--- src/calendar/Calendar.js
+++ src/calendar/Calendar.js
@@ -1,13 +1,94 @@
 import React from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import interactionPlugin from "@fullcalendar/interaction";
 import './Calendar.css'
+import { getConsultationsForProfessorsCalendar } from '../util/APIUtils'
+import Popup from '../common/Popup';
 
 export default class Calendar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      consultations: [],
+      professorsId: 1225652125495,
+      calendarId: 1,
+      showPopup: false
+    }
+  }
+  togglePopup() {
+    this.setState({
+      showPopup: !this.state.showPopup
+    });
+  }
+  componentDidMount() {
+    this.loadConsultations(this.state.professorsId, this.state.calendarId);
+  }
+
+  loadConsultations(professorsId, calendarId) {
+    getConsultationsForProfessorsCalendar(professorsId, calendarId).then(function (result) {
+      result.forEach(element => {
+        if (this.didAppointmentDatePass(element)) {
+          this.setState({
+            consultations: [...this.state.consultations, {
+              title: 'Termin has passed',
+              start: element.datumIVremePocetka,
+              dogadjajPK: element.dogadjajPK,
+              color: 'red'
+            }]
+          });
+        } else {
+          this.setState({
+            consultations: [...this.state.consultations, {
+              title: 'Free termin',
+              start: element.datumIVremePocetka,
+              end: element.datumIVremeZavrsetka,
+              dogadjajPK: element.dogadjajPK,
+              color: 'green'
+            }]
+          });
+        }
+
+      })
+    }.bind(this));
+  }
+
+
+  didAppointmentDatePass(dogadjaj) {
+    if (new Date(dogadjaj.datumIVremePocetka) < new Date()) {
+      return true;
+    }
+    return false;
+  };
+
+  handleEventClick = (arg) => { // bind with an arrow function
+    alert("EVENT CLICKED")
+  }
+
+  handleDayClick = (arg) => { // bind with an arrow function
+    alert("DAY CLICKED " + arg.dateStr)
+  }
 
   render() {
     return (
-      <FullCalendar defaultView="dayGridMonth" plugins={[ dayGridPlugin ]} />
+      <div>
+        <FullCalendar
+          dateClick={this.togglePopup.bind(this)}
+          eventClick={this.handleEventClick}
+          defaultView="dayGridMonth"
+          plugins={[dayGridPlugin, interactionPlugin]}
+          events={
+            this.state.consultations
+          }
+        />
+        {this.state.showPopup ?
+          <Popup
+            text='Click "Close Button" to hide popup'
+            closePopup={this.togglePopup.bind(this)}
+          />
+          : null
+        }
+      </div>
     )
   }
 
--- src/util/APIUtils.js
+++ src/util/APIUtils.js
@@ -56,3 +56,59 @@ export function checkUsernameAvailability(username) {
         method: 'GET'
     });
 }
+
+export function getConsultationsForProfessorsCalendar(professorsId, calendarId) {
+    return request({
+        url: "http://localhost:8083/konsultacije/zaNastavnikovKalendar?jmbg=" + professorsId + "&idKalendara=" + calendarId,
+        type: "GET",
+        async: false,
+        contentType: "application/json",
+        processData: false // avoid the data being parsed to query string params
+    });
+}
+
+
+
+
+// export function dodajKonsultaciju() {
+//     return request({
+//         url: API_BASE_URL + "konsultacije/add",
+//         method: 'POST',
+//         async: false,
+//         data: JSON.stringify(self.kreirajKonsultacije()),
+//         contentType: "application/json"
+
+
+//     });
+// }
+// function kreirajKonsultacije() {
+//     return {
+//         'dogadjajPK': JSON.parse("{\"idKalendara\": " + 1 + "}"),
+//         'datumIVremePocetka': self.datumIVremePocetka(),
+//         'datumIVremeZavrsetka': self.datumIVremeZavrsetka(),
+//         'mestoOdrzavanja': self.mestoOdrzavanja(),
+//         'idTipaDogadjaja': tipDogadjaja,
+//         'kapacitet': self.kapacitet(),
+//         'brojZakazanih': 0
+//     };
+// };
+// self.dodajKonsultaciju = function () {
+//     $.ajax({
+//         url: "http://localhost:8083/konsultacije/add",
+//         method: "POST",
+//         async: false,
+//         data: JSON.stringify(self.kreirajKonsultacije()),
+//         contentType: "application/json",
+//         success: function (result, status, jqXHR) {
+//             rootViewModel.poruka("Sistem je zapamtio konsultacije");
+//             document.querySelector('#dijalogPoruka').open();
+//             self.osveziKalendar();
+//         },
+//         error: function (jqXHR, textStatus, errorThrown) {
+//             rootViewModel.poruka("Sistem ne može da zapamti novi termin konsultacija");
+//             document.querySelector('#dijalogPoruka').open();
+//             console.log('Greska u funkciji login: ' + textStatus);
+//         }
+//     });
+
+// };
